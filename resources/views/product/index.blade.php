cd25ce2a8eeb8c5da2b6bac8702698ce8d741e3d
Tu <anhtunhatminh2711@gmail.com>
2026-01-29 20:01:52 +0700

    noi dung lab 1

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,12 @@
+    <!DOCTYPE html>
+<html>
+<body>
+    <h1>Danh sách sản phẩm mẫu</h1>
+    <ul>
+        <li>Laptop Gaming Asus</li>
+        <li>Chuột Logitech</li>
+        <li>Bàn phím cơ</li>
+    </ul>
+    <a href="{{ route('product.add') }}"><button>Thêm mới sản phẩm</button></a>
+</body>
+</html>
